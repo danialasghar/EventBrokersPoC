659c024628bf61a858d483ca9e0fad56ae7c39cf
Yuriy Poslavskyy <yuriy.poslavskyy@cra-arc.gc.ca>
2022-06-24 14:49:08 -0400

    Remmed out Router main

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeoutException;
 
 public class Router {
 
+	/*
     private static int instanceID;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -57,4 +58,5 @@ public class Router {
                 break;
         }
     }
+    */
 }
