c4e0e342ee9fb19aba7b57ccb2d1878d6a8750bf
Ubuntu <czf243@CZF243.qvtl33ixhedebhqfrecyyvrqpg.ux.internal.cloudapp.net>
2022-06-27 22:03:26 +0000

    ibm producer code

---
+++ event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/dependencies/LoggingHelper.java
@@ -0,0 +1,21 @@
+
+import java.util.logging.*;
+
+public class LoggingHelper {
+    private static final Level LOGLEVEL = Level.ALL;
+
+    public static void init(Logger logger) {
+        Logger defaultLogger = Logger.getLogger("");
+        Handler[] handlers = defaultLogger.getHandlers();
+        if (handlers != null && handlers.length > 0) {
+            defaultLogger.removeHandler(handlers[0]);
+        }
+
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(LOGLEVEL);
+        logger.addHandler(consoleHandler);
+
+        logger.setLevel(LOGLEVEL);
+        logger.finest("Logging initialised");
+    }
+}
---
+++ event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/sources/IbmMQ.java
@@ -0,0 +1,81 @@
+import java.util.logging.*;
+import javax.jms.Destination;
+import javax.jms.JMSProducer;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.JMSRuntimeException;
+
+
+public class IbmMQ {
+    private static final Logger logger = Logger.getLogger("com.ibm.mq.samples.jms");
+
+    public static final String PRODUCER_PUT = "queue";
+    public static final String PRODUCER_PUB = "topic";
+
+    private JMSContext context = null;
+    private Destination destination = null;
+    private JMSProducer producer = null;
+    private ConnectionHelper ch = null;
+
+    public BasicProducer(String type) {
+        String id = null;
+
+        switch(type){
+            case PRODUCER_PUT :
+                id = "Basic put";
+                break;
+            case PRODUCER_PUB :
+                id = "Basic pub";
+                break;
+        }
+
+        LoggingHelper.init(logger);
+        logger.info("Sub application is starting");
+
+        ch = new ConnectionHelper(id, ConnectionHelper.USE_CONNECTION_STRING);
+        logger.info("created connection factory");
+
+        context = ch.getContext();
+        logger.info("context created");
+
+        switch(type){
+            case PRODUCER_PUB :
+                destination = ch.getTopicDestination();
+                break;
+            case PRODUCER_PUT :
+                destination = ch.getDestination();
+                break;
+        }
+
+        // Set so no JMS headers are sent.
+        ch.setTargetClient(destination);
+
+        logger.info("destination created");
+
+        producer = context.createProducer();
+    }
+
+    public void send(String message, int n_messages) {
+        for (int i = 0; i < n_messages; i++) {
+            logger.info("Publishing messages.\n");
+
+            try {
+                producer.send(destination, message);
+                logger.info("message was sent");
+                Thread.sleep(2000);
+            } catch (JMSRuntimeException jmsex) {
+                jmsex.printStackTrace();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void close() {
+        ch.closeContext();
+        ch = null;
+    }
+}
