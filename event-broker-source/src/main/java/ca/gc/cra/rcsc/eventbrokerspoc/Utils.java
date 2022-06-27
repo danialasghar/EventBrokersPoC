4a2543675c6fb3d4bd68a709f442f22f2d40cd07
Yuriy Poslavskyy <yuriy.poslavskyy@cra-arc.gc.ca>
2022-06-27 15:32:40 -0400

    Updated message to include miliseconds

--- event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/Utils.java
+++ event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/Utils.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 public class Utils {
 
 	public static String buildMessage(int instanceID) {
-		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new java.util.Date());
         
 		return "Instance Number: " + Integer.toString(instanceID) + " , Sent at: " + timeStamp;
 	}
