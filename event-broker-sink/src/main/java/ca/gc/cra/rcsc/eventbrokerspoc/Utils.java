edb03caf5cec164d41b1518be17a910d87e46fad
Yuriy Poslavskyy <yuriy.poslavskyy@cra-arc.gc.ca>
2022-06-29 09:11:55 -0400

    Updated sink utils

--- event-broker-sink/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/Utils.java
+++ event-broker-sink/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/Utils.java
@@ -1,14 +1,17 @@
 package ca.gc.cra.rcsc.eventbrokerspoc;
 
 public class Utils {
+	
+	public static final String CONNECTED_STRING = "connected";
+	public static final String DISCONNECTED_STRING = "disconnected";
 
 	public static String isConnectedNullCheck(Object obj) {
 		String result = "";
 		
 		if (obj != null) {
-			result += "connected";
+			result += CONNECTED_STRING;
 		} else {
-			result += "disconnected";
+			result += DISCONNECTED_STRING;
 		}
 		
 		return result;
