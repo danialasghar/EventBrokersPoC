dea291e9b1bbbadbc60370c0fa4dc5d8131b7716
Yuriy Poslavskyy <yuriy.poslavskyy@cra-arc.gc.ca>
2022-07-11 15:29:29 -0400

    Updated ArtemisMQ to use Injection

--- event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/sources/ActiveMQ.java
+++ event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/sources/ActiveMQ.java
@@ -1,15 +1,22 @@
 package ca.gc.cra.rcsc.eventbrokerspoc.sources;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import ca.gc.cra.rcsc.eventbrokerspoc.Utils;
 
-
+@ApplicationScoped
 public class ActiveMQ {
+    @Channel("new-artemis")
+    Emitter<String> messageEmitter;
+
     private int instanceId;
     
-    @Channel("new-artemis") Emitter<String> messageEmitter;
+    public ActiveMQ() {
+        this(4);
+    }
 
     public ActiveMQ(int instanceId) {
         this.instanceId = instanceId;
@@ -17,6 +24,7 @@ public class ActiveMQ {
 
     public void sendMessage() {
         String message = Utils.buildMessage(instanceId);
+
         messageEmitter.send(message);
 
         System.out.println("ActiveMQ-MESSAGE SENT: " + message);
