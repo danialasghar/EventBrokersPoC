45033493d13e04e75bbaf840141983e0f0f8e97f
Danial Asghar <46968467+danialasghar@users.noreply.github.com>
2022-07-12 13:12:51 +0000

    try admin user

--- event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/sources/IbmMQ.java
+++ event-broker-source/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/sources/IbmMQ.java
@@ -22,7 +22,7 @@ public class IbmMQ {
     private static final int PORT = 1414; // Listener port for your queue manager
     private static final String CHANNEL = "DEV.APP.SVRCONN"; // Channel name
     private static final String QMGR = "QM1"; // Queue manager name
-    private static final String APP_USER = "app"; // User name that application uses to connect to MQ
+    private static final String APP_USER = "admin"; // User name that application uses to connect to MQ
     private static final String APP_PASSWORD = "passw0rd"; // Password that the application uses to connect to MQ
     private static final String QUEUE_NAME = "DEV.QUEUE.1"; // Queue that the application uses to put and get messages to and from
 
